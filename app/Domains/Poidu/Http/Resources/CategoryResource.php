2d34b04342cdcd48e1c94bb3afeff2556a38817a
Сергей Лешуков <s.m.leshukov@yandex.ru>
2026-02-20 18:35:22 +0700

    feat: добавление счётчика событий к категориям

--- app/Domains/Poidu/Http/Resources/CategoryResource.php
+++ app/Domains/Poidu/Http/Resources/CategoryResource.php
@@ -17,6 +17,7 @@ class CategoryResource extends JsonResource
         return [
             "id" => $this->id,
             "name" => $this->category,
+            "count" => $this->events->count(),
         ];
     }
 }
