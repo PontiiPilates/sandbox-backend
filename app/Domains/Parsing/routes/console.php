dae6fab97c863e7cc26cdea2856dadcb03b91e32
PontiiPilates <s.m.leshukov@yandex.ru>
2026-03-08 18:36:42 +0700

    feat: добавление автоматизации процессов поступления данных

---
+++ app/Domains/Parsing/routes/console.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schedule;
+
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->purpose('Display an inspiring quote');
+
+// UTC > KRSK
+//  00 > 07
+//  01 > 08
+//  02 > 09 -
+//  03 > 10
+//  04 > 11
+//  05 > 12
+//  06 > 13
+//  07 > 14
+//  08 > 15 -
+//  09 > 16
+//  10 > 17
+//  11 > 18
+//  12 > 19
+//  13 > 20
+//  14 > 21
+//  15 > 22
+//  16 > 23
+//  17 > 00
+//  18 > 01
+//  19 > 02
+//  20 > 03
+//  21 > 04
+//  22 > 05
+//  23 > 06
+
+Schedule::command('parsing:tg-events-extract 1')->twiceDailyAt(1, 7, 50);
+Schedule::command('parsing:extraction-preparation')->twiceDailyAt(1, 7, 55);
+Schedule::command('parsing:update-events')->twiceDaily(2, 8);
--- app/Domains/Parsing/src/ParsingServiceProvider.php
+++ app/Domains/Parsing/src/ParsingServiceProvider.php
@@ -31,6 +31,7 @@ class ParsingServiceProvider extends ServiceProvider
     {
         $this->mergeConfigFrom(realpath(__DIR__ . '/../config/services.php'), 'services');
         $this->loadRoutesFrom(realpath(__DIR__ . '/../routes/api.php'));
+        $this->loadRoutesFrom(realpath(__DIR__ . '/../routes/console.php'));
         $this->loadMigrationsFrom(realpath(__DIR__ . '/../database/migrations'));
         $this->loadTranslationsFrom(realpath(__DIR__ . '/../lang'), 'lang');
     }
