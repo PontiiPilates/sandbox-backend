482ae4df62cf321478e5095e3a2e089a597c4509
PontiiPilates <s.m.leshukov@yandex.ru>
2026-04-28 07:53:50 +0700

    feat: добавление ЯндексМетрики

--- app/Domains/Poidu/App/resources/views/layouts/app.blade.php
+++ app/Domains/Poidu/App/resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <meta name="author" content="Poidu">
     <meta name="description" content="">
     <meta name="keywords" content="" />
-    <link rel="canonical" href="{{ request()->url() }}"/>
+    <link rel="canonical" href="{{ request()->url() }}" />
 
     <!-- Favicon -->
     <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('volt/assets/img/favicon/apple-touch-icon.png') }}">
@@ -33,6 +33,37 @@
 
     <!-- Yandex -->
     <meta name="yandex-verification" content="b1f15c0553709342" />
+
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function(m, e, t, r, i, k, a) {
+            m[i] = m[i] || function() {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) {
+                    return;
+                }
+            }
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+        })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=106951435', 'ym');
+
+        ym(106951435, 'init', {
+            ssr: true,
+            webvisor: true,
+            clickmap: true,
+            ecommerce: "dataLayer",
+            referrer: document.referrer,
+            url: location.href,
+            accurateTrackBounce: true,
+            trackLinks: true
+        });
+    </script>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/106951435" style="position:absolute; left:-9999px;" alt="" /></div>
+    </noscript>
+    <!-- /Yandex.Metrika counter -->
 </head>
 
 <body class="">
